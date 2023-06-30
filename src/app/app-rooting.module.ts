b196193d974add33b0224b9508791bea3397ef46
Theo Pierson <thheopierson@gmail.com>
2023-06-30 11:31:41 +0200

    feat: add landing page

--- src/app/app-rooting.module.ts
+++ src/app/app-rooting.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailContactComponent } from './detail-contact/detail-contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [{}];
+const routes: Routes = [
+  { path: 'detail-contact', component: DetailContactComponent },
+  { path: '', component: LandingPageComponent },
+];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRootingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { CardComponent } from './card/card.component';
 import { ArrayComponent } from './array/array.component';
 import { DetailContactComponent } from './detail-contact/detail-contact.component';
 import { AppRootingModule } from './app-rooting.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, ArrayComponent, DetailContactComponent],
+  declarations: [AppComponent, CardComponent, ArrayComponent, DetailContactComponent, LandingPageComponent],
   imports: [BrowserModule, AppRootingModule],
   providers: [],
   bootstrap: [AppComponent],
